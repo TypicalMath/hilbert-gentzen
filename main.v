816f85535c65753a7b602c2275203effee00eff8
Alireza Mahmoudian <mahmoudian@aut.ac.ir>
2022-07-06 14:45:50 +0430

    just some cleaning

--- main.v
+++ main.v
@@ -18,23 +18,19 @@ Definition top := ^~^F.
 Notation "^T" := top (at level 0).
 Definition iff x y := (x ^> y) ^& (y ^> x).
 Notation "x <^> y" := (iff x y) (at level 13, right associativity).
-        
+
 Reserved Notation "C |-n f" (at level 20).
 
 Inductive ND : Ensemble Formula -> Formula -> Prop :=
-    | ax (C:Ensemble Formula) (f: Formula) (H:In Formula C  f) : C |-n f
-    | conjE1 (C:Ensemble Formula) (f1 f2: Formula) (H: C |-n (f1 ^& f2)) : C |-n f1
-    | conjE2 (C:Ensemble Formula) (f1 f2: Formula) (H: C |-n (f1 ^& f2)) : C |-n f2
-    | conjI (C:Ensemble Formula) (f1 f2: Formula) (H1: C |-n f1) (H2: C |-n f2) : C |-n (f1 ^& f2)
-    | disjE (C:Ensemble Formula) (f1 f2 f:Formula) (H1: (Union Formula C (Singleton Formula f1)) |-n f) 
-        (H2: (Union Formula C (Singleton Formula f2)) |-n f) (H3: C |-n (f1 ^| f2) ): C |-n f 
-    | disjI1 (C:Ensemble Formula) (f1 f2:Formula) (H:C |-n f1): C |-n (f1 ^| f2)
-    | disjI2 (C:Ensemble Formula) (f1 f2:Formula) (H:C |-n f2): C |-n (f1 ^| f2)
-    | impE (C:Ensemble Formula) (f1 f2:Formula) (H1: C |-n (f1 ^> f2)) (H2: C |-n f1): C |-n f2
-    | impI (C:Ensemble Formula) (f1 f2:Formula) (H: (Union Formula C (Singleton Formula f1)) |-n f2): C |-n (f1 ^> f2)
-    | botE (C:Ensemble Formula) (f:Formula) (H: C |-n ^F): C |-n f
+    | ax (C : Ensemble Formula) (f : Formula) (H : In Formula C  f) : C |-n f
+    | conjE1 (C : Ensemble Formula) (f1 f2 : Formula) (H : C |-n (f1 ^& f2)) : C |-n f1
+    | conjE2 (C : Ensemble Formula) (f1 f2 : Formula) (H : C |-n (f1 ^& f2)) : C |-n f2
+    | conjI (C : Ensemble Formula) (f1 f2 : Formula) (H1 : C |-n f1) (H2 : C |-n f2) : C |-n (f1 ^& f2)
+    | disjE (C : Ensemble Formula) (f1 f2 f : Formula) (H1 : (Union Formula C (Singleton Formula f1)) |-n f) 
+        (H2 : (Union Formula C (Singleton Formula f2)) |-n f) (H3 : C |-n (f1 ^| f2) ) : C |-n f 
+    | disjI1 (C : Ensemble Formula) (f1 f2 : Formula) (H : C |-n f1) : C |-n (f1 ^| f2)
+    | disjI2 (C : Ensemble Formula) (f1 f2 : Formula) (H : C |-n f2) : C |-n (f1 ^| f2)
+    | impE (C : Ensemble Formula) (f1 f2 : Formula) (H1 : C |-n (f1 ^> f2)) (H2 : C |-n f1) : C |-n f2
+    | impI (C : Ensemble Formula) (f1 f2 : Formula) (H : (Union Formula C (Singleton Formula f1)) |-n f2) : C |-n (f1 ^> f2)
+    | botE (C : Ensemble Formula) (f : Formula) (H : C |-n ^F): C |-n f
     where "x |-n y" := (ND x y).
-
-
-
-
